a2cb8b45e4dcc6a64a29cc5c434b4d22558327c0
Dimas Pratama <yohanesdimas888@gmail.com>
2024-07-09 13:04:14 +0700

    Update

--- bencana-api/sequelize.js
+++
@@ -1,9 +0,0 @@
-// sequelize.js
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
