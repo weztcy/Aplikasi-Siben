a13bbe2aafa21f6223dc8ebeaad3c8788eb2092e
Dimas Pratama <92010124+weztcy@users.noreply.github.com>
2024-07-16 23:18:29 +0700

    Update

--- src/Component/EditDataBencana.js
+++ src/Component/EditDataBencana.js
@@ -65,8 +65,10 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={handleSubmitEdit}>
-          {/* Form fields */}
-          <Form.Group controlId="tanggalBencana">
+          <Form.Group
+            style={{ marginBottom: "10px" }}
+            controlId="tanggalBencana"
+          >
             <Form.Label>Tanggal Bencana</Form.Label>
             <Form.Control
               type="date"
@@ -75,7 +77,7 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Form.Group controlId="provinsi">
+          <Form.Group style={{ marginBottom: "10px" }} controlId="provinsi">
             <Form.Label>Provinsi</Form.Label>
             <Form.Control
               as="select"
@@ -84,51 +86,54 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             >
               <option value="">Pilih Provinsi</option>
-                <option value="Bali">Bali</option>
-                <option value="Bangka Belitung">Bangka Belitung</option>
-                <option value="Banten">Banten</option>
-                <option value="Bengkulu">Bengkulu</option>
-                <option value="Daerah Istimewa Yogyakarta">
-                  Daerah Istimewa Yogyakarta
-                </option>
-                <option value="DKI Jakarta">DKI Jakarta</option>
-                <option value="Gorontalo">Gorontalo</option>
-                <option value="Jambi">Jambi</option>
-                <option value="Jawa Barat">Jawa Barat</option>
-                <option value="Jawa Tengah">Jawa Tengah</option>
-                <option value="Jawa Timur">Jawa Timur</option>
-                <option value="Kalimantan Barat">Kalimantan Barat</option>
-                <option value="Kalimantan Selatan">Kalimantan Selatan</option>
-                <option value="Kalimantan Tengah">Kalimantan Tengah</option>
-                <option value="Kalimantan Timur">Kalimantan Timur</option>
-                <option value="Kalimantan Utara">Kalimantan Utara</option>
-                <option value="Kepulauan Riau">Kepulauan Riau</option>
-                <option value="Lampung">Lampung</option>
-                <option value="Maluku">Maluku</option>
-                <option value="Maluku Utara">Maluku Utara</option>
-                <option value="Nanggroe Aceh Darussalam">
-                  Nanggroe Aceh Darussalam
-                </option>
-                <option value="Nusa Tenggara Barat">Nusa Tenggara Barat</option>
-                <option value="Nusa Tenggara Timur">Nusa Tenggara Timur</option>
-                <option value="Papua">Papua</option>
-                <option value="Papua Barat">Papua Barat</option>
-                <option value="Papua Barat Daya">Papua Barat Daya</option>
-                <option value="Papua Pegunungan">Papua Pegunungan</option>
-                <option value="Papua Selatan">Papua Selatan</option>
-                <option value="Papua Tengah">Papua Tengah</option>
-                <option value="Riau">Riau</option>
-                <option value="Sulawesi Barat">Sulawesi Barat</option>
-                <option value="Sulawesi Selatan">Sulawesi Selatan</option>
-                <option value="Sulawesi Tengah">Sulawesi Tengah</option>
-                <option value="Sulawesi Tenggara">Sulawesi Tenggara</option>
-                <option value="Sulawesi Utara">Sulawesi Utara</option>
-                <option value="Sumatera Barat">Sumatera Barat</option>
-                <option value="Sumatera Selatan">Sumatera Selatan</option>
-                <option value="Sumatera Utara">Sumatera Utara</option>
+              <option value="Bali">Bali</option>
+              <option value="Bangka Belitung">Bangka Belitung</option>
+              <option value="Banten">Banten</option>
+              <option value="Bengkulu">Bengkulu</option>
+              <option value="Daerah Istimewa Yogyakarta">
+                Daerah Istimewa Yogyakarta
+              </option>
+              <option value="DKI Jakarta">DKI Jakarta</option>
+              <option value="Gorontalo">Gorontalo</option>
+              <option value="Jambi">Jambi</option>
+              <option value="Jawa Barat">Jawa Barat</option>
+              <option value="Jawa Tengah">Jawa Tengah</option>
+              <option value="Jawa Timur">Jawa Timur</option>
+              <option value="Kalimantan Barat">Kalimantan Barat</option>
+              <option value="Kalimantan Selatan">Kalimantan Selatan</option>
+              <option value="Kalimantan Tengah">Kalimantan Tengah</option>
+              <option value="Kalimantan Timur">Kalimantan Timur</option>
+              <option value="Kalimantan Utara">Kalimantan Utara</option>
+              <option value="Kepulauan Riau">Kepulauan Riau</option>
+              <option value="Lampung">Lampung</option>
+              <option value="Maluku">Maluku</option>
+              <option value="Maluku Utara">Maluku Utara</option>
+              <option value="Nanggroe Aceh Darussalam">
+                Nanggroe Aceh Darussalam
+              </option>
+              <option value="Nusa Tenggara Barat">Nusa Tenggara Barat</option>
+              <option value="Nusa Tenggara Timur">Nusa Tenggara Timur</option>
+              <option value="Papua">Papua</option>
+              <option value="Papua Barat">Papua Barat</option>
+              <option value="Papua Barat Daya">Papua Barat Daya</option>
+              <option value="Papua Pegunungan">Papua Pegunungan</option>
+              <option value="Papua Selatan">Papua Selatan</option>
+              <option value="Papua Tengah">Papua Tengah</option>
+              <option value="Riau">Riau</option>
+              <option value="Sulawesi Barat">Sulawesi Barat</option>
+              <option value="Sulawesi Selatan">Sulawesi Selatan</option>
+              <option value="Sulawesi Tengah">Sulawesi Tengah</option>
+              <option value="Sulawesi Tenggara">Sulawesi Tenggara</option>
+              <option value="Sulawesi Utara">Sulawesi Utara</option>
+              <option value="Sumatera Barat">Sumatera Barat</option>
+              <option value="Sumatera Selatan">Sumatera Selatan</option>
+              <option value="Sumatera Utara">Sumatera Utara</option>
             </Form.Control>
           </Form.Group>
-          <Form.Group controlId="kotaKabupaten">
+          <Form.Group
+            style={{ marginBottom: "10px" }}
+            controlId="kotaKabupaten"
+          >
             <Form.Label>Kota / Kabupaten</Form.Label>
             <Form.Control
               type="text"
@@ -137,7 +142,7 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Form.Group controlId="alamatLokasi">
+          <Form.Group style={{ marginBottom: "10px" }} controlId="alamatLokasi">
             <Form.Label>Alamat Lokasi</Form.Label>
             <Form.Control
               type="text"
@@ -146,7 +151,7 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Form.Group controlId="bencanaAlam">
+          <Form.Group style={{ marginBottom: "10px" }} controlId="bencanaAlam">
             <Form.Label>Bencana Alam</Form.Label>
             <Form.Control
               as="select"
@@ -155,19 +160,22 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             >
               <option value="">Pilih Bencana</option>
-                <option value="Gempa Bumi">Gempa Bumi</option>
-                <option value="Tsunami">Tsunami</option>
-                <option value="Gunung Meletus">Gunung Meletus</option>
-                <option value="Banjir">Banjir</option>
-                <option value="Tanah Longsor">Tanah Longsor</option>
-                <option value="Angin Topan">Angin Topan</option>
-                <option value="Kekeringan">Kekeringan</option>
-                <option value="Kebakaran Hutan">Kebakaran Hutan</option>
-                <option value="Gelombang Panas">Gelombang Panas</option>
-                <option value="Badai Petir">Badai Petir</option>
+              <option value="Gempa Bumi">Gempa Bumi</option>
+              <option value="Tsunami">Tsunami</option>
+              <option value="Gunung Meletus">Gunung Meletus</option>
+              <option value="Banjir">Banjir</option>
+              <option value="Tanah Longsor">Tanah Longsor</option>
+              <option value="Angin Topan">Angin Topan</option>
+              <option value="Kekeringan">Kekeringan</option>
+              <option value="Kebakaran Hutan">Kebakaran Hutan</option>
+              <option value="Gelombang Panas">Gelombang Panas</option>
+              <option value="Badai Petir">Badai Petir</option>
             </Form.Control>
           </Form.Group>
-          <Form.Group controlId="penjelasanPenyebab">
+          <Form.Group
+            style={{ marginBottom: "10px" }}
+            controlId="penjelasanPenyebab"
+          >
             <Form.Label>Penjelasan Penyebab</Form.Label>
             <Form.Control
               type="text"
@@ -176,7 +184,7 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Form.Group controlId="korbanJiwa">
+          <Form.Group style={{ marginBottom: "10px" }} controlId="korbanJiwa">
             <Form.Label>Korban Jiwa</Form.Label>
             <Form.Control
               type="number"
@@ -185,8 +193,12 @@ function EditDataBencana({ showEditModal, handleCloseEdit, editData }) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <Button
+            variant="primary"
+            type="submit"
+            style={{ width: "100%", marginTop: "20px", height: "60px" }}
+          >
+            Simpan Perubahan
           </Button>
         </Form>
       </Modal.Body>
